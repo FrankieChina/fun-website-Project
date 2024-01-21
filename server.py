fdfdc11f3d7bf360b3eeb17c8f02afc70a743d1d
Frankie <frankiechina@outlook.com>
2024-01-21 09:52:33 -0700

    frankie version

--- server.py
+++ server.py
@@ -7,28 +7,28 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     current_year = datetime.datetime.now().year
-    return render_template("index.html", current_year=current_year)
+    return render_template("Home.html", current_year=current_year)
 
 
-@app.route("/start")
-def get_start_page():
-    return render_template("start.html")
+@app.route("/page_2")
+def get_page_2():
+    return render_template("page2.html")
 
 
-@app.route("/user/<username>")
-def user(username):
-    return (f'<h1 style="text-align: center"> Hi  {username}!</h1>')
 
+@app.route("/Home/persistent")
+def get_persistent_page():
+    return render_template("persistent.html")
 
 @app.route("/start/backUp")
 def get_backup_page():
     return render_template("backUp.html")
 
 
-@app.route("/start/miracle")
+@app.route("/Home/miracle")
 def get_miracle_page():
     return render_template("miracle.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=80,debug=True)
